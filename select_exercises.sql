3738c695e43721a7bfd351c36d31644d29817349
Steve Newton <newton36@gmail.com>
2021-06-23 10:48:21 -0500

    more stuff

---
+++ select_exercises.sql
@@ -0,0 +1,20 @@
+USE albums_db;
+
+SELECT * FROM albums
+	WHERE artist = 'Pink Floyd';
+	
+SELECT release_date FROM albums
+	WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
+SELECT genre FROM albums
+	WHERE name = 'Nevermind';
+	
+SELECT * FROM albums
+	WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT * FROM albums
+	WHERE sales < 20;
+	
+
+SELECT * FROM albums
+	WHERE genre = 'Rock';	
