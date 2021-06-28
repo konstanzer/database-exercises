25856fa77b90396415e2d2a103bde43fedd6ccb1
Steve Newton <newton36@gmail.com>
2021-06-28 15:24:58 -0500

    more yet

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,15 +1,47 @@
 use employees;
 
-select count(distinct(title)) from titles;
+select count(distinct(title)) 'titles' from titles;
+#3
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+#4
+SELECT first_name, last_name, COUNT(last_name) 'count'
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name
+ORDER BY count DESC;
+#5-6 chleq, lindqvist, qiwen
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%'
+GROUP BY last_name;
+#7
+SELECT first_name, gender, COUNT(first_name) 'count'
+FROM employees
+WHERE first_name IN  ('Irena', 'Vidya', 'Maya')
+GROUP BY first_name, gender
+ORDER BY count DESC;
 
-#899 rows
-SELECT * FROM employees
-	WHERE last_name LIKE 'E%E';
-#1,873 rows
-SELECT * FROM employees
-	WHERE last_name LIKE '%Q%';
+#8: 285872
+SET @u := (SELECT lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4),
+			'_', substr(birth_date, 6, 2), substr(birth_date, 3, 2))) FROM employees);
 
-#547 rows
-SELECT * FROM employees
-	WHERE last_name LIKE '%Q%'
-	AND last_name NOT LIKE '%QU%';
+SELECT DISTINCT(lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4),
+			'_', substr(birth_date, 6, 2), substr(birth_date, 3, 2)))) 'username',
+		COUNT(lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4),
+			'_', substr(birth_date, 6, 2), substr(birth_date, 3, 2)))) 'total'
+FROM employees
+GROUP BY username
+ORDER BY total DESC;
+
+#B: 300024 - 285872 = 14152 duplicates
+SELECT DISTINCT(lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4),
+			'_', substr(birth_date, 6, 2), substr(birth_date, 3, 2)))) 'username',
+		COUNT(lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4),
+			'_', substr(birth_date, 6, 2), substr(birth_date, 3, 2)))) 'total'
+FROM employees;
+GROUP BY username
+HAVING total > 1
+ORDER BY total DESC;
