fa3b965a228ccfd2303d19e4eea2867436c67891
Steve Newton <newton36@gmail.com>
2021-06-30 13:15:03 -0500

    booyaka

--- subq_exercises.sql
+++ subq_exercises.sql
@@ -5,45 +5,62 @@ select emp_no, last_name from employees
 where hire_date = (
 		select hire_date from employees
 		where emp_no = '101010');
-		
-#2 is there an A-A-mod up in here? no subquery needed
+
+	
+#2 is there an A-A-mod? no subquery needed
 #subquery only makes sense to me when you have to run a query in order to run the query
-#potentially uncovering first name discrimination
 
 select distinct(title) from employees
 	join titles t using(emp_no)
 	join dept_emp de using(emp_no)
 where first_name = 'Aamod' AND de.to_date > current_date;
-		
-#3 91,479 ghosts
-select emp_no, to_date, last_name from employees
-join dept_emp using(emp_no)
-where emp_no IN (
-		select emp_no from employees
-		where to_date < current_date);
-		
-#4, bonus1 four women managers but no subquery
+
+
+#3. 300,024 peons - 240,124 working now = 59,900 dead peons
+select count(*) - (select count(emp_no) from employees
+						join dept_emp using(emp_no)
+							where emp_no in (select emp_no from employees
+												where to_date > current_date)) ghosts
+from employees;
+
+
+#4, bonus1 four women managers, alas, no subquery
 select dept_name, last_name, gender from dept_manager
 	join employees using(emp_no)
 	join departments using(dept_no)
 where to_date > current_date AND gender = 'F';
 
-#5 are you above average? ~$64k
+
+#5 above average payday
 select emp_no, last_name, salary from employees
 	join salaries using(emp_no)
 where to_date > current_date
-	AND salary > (
-		select avg(salary) from salaries)
-ORDER BY salary;
+	and salary > (select avg(salary) from salaries) #historical average
+order by salary;
 
-#6:  -sigma+x < x < sigma+x w/ stdev ~16.9k
-# it is ~78 percent, 163k/(300k-91k)
-select emp_no, salary from employees
-	join salaries using(emp_no)
-where to_date > current_date
-	AND salary BETWEEN (select avg(salary)-std(salary) from salaries)
-		AND (select avg(salary)+std(salary) from salaries)
-ORDER BY salary DESC;
+
+#6:  count(x) > max-sigma = 83
+select count(salary)
+from salaries where to_date > now()
+	and salary > (select max(salary)-std(salary)
+					from salaries where to_date > now());
+#0.03% of all peons
+select (select count(salary) from salaries where to_date > now()
+			and salary > (select max(salary)-std(salary)
+								from salaries where to_date > now())) /
+		(select count(salary)from salaries where to_date > now()) barons;
+####
+
+select * from (select std(salary) from salaries where to_date > now()) std;
+
+select (select max(salary) from salaries) as max, min(salary) as min from salaries;
+
+#??
+select count(salary), (select std(salary) from salaries where to_date > now()) stdev,
+		(select avg(salary) from salaries where to_date > now()) average,
+from salaries where to_date > now() and salary between (average-stdev) and (average+stdev);
+
+select avg(salary)-std(salary), avg(salary)+std(salary) from salaries where to_date > now();
 
 #B2-3 Find the dept. & name of the employee with the highest salary.
 select dept_name, last_name, first_name, salary from employees
