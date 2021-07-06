89287b58c9d3def112091046042e980055bb57de
Steve Newton <newton36@gmail.com>
2021-07-06 15:40:03 -0500

    more cowbell

--- more_exercises.sql
+++ more_exercises.sql
@@ -13,9 +13,31 @@ join (select dept_no, avg(salary) dept_mean
 where s.to_date > current_date and dm.to_date > current_date
 order by diff;
 
+use world;
+#2
+select language, percentage from countrylanguage
+join city using(countrycode)
+where name = 'Santa Monica';
+#3
+select count(name), region from country
+group by region;
+#4
+select sum(population), region from country
+group by region;
+#5
+select sum(population), continent from country
+group by continent;
+#6 --this is wrong, should be weighted by population
+select avg(lifeexpectancy) from country;
+#7
+select avg(lifeexpectancy) life, continent from country
+group by continent
+order by life;
+#B
 
-select dept_no, avg(salary) dept_mean
-	from salaries
-		join dept_emp using(emp_no)
-	where salaries.to_date > current_date
-	group by dept_no
+use sakila;
+
+
+
+
+
