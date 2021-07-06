173453cb3a058912e86461bb166fa628d9719355
Steve Newton <newton36@gmail.com>
2021-07-06 15:22:52 -0500

    huh

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,1 +1,21 @@
 use employees;
+#1
+select last_name, d.dept_name, salary-dept_mean diff
+from employees
+join salaries s using(emp_no)
+join dept_manager dm using(emp_no)
+join departments d using(dept_no)
+join (select dept_no, avg(salary) dept_mean
+			from salaries
+				join dept_emp using(emp_no)
+			where salaries.to_date > current_date
+			group by dept_no) mean_sal using(dept_no)
+where s.to_date > current_date and dm.to_date > current_date
+order by diff;
+
+
+select dept_no, avg(salary) dept_mean
+	from salaries
+		join dept_emp using(emp_no)
+	where salaries.to_date > current_date
+	group by dept_no
